0a5ab7a2edd33c28d13bbb67c758cdd6a6dab509
olivia liau <65891724+oleeveeuh@users.noreply.github.com>
2026-02-03 10:47:13 -0800

    Modify ASCII art in update_readme.py
    Updated ASCII art in the README file.

--- update_readme.py
+++ update_readme.py
@@ -20,7 +20,8 @@ YOUR_SPECIALTY = "Healthcare ML, Time Series Forecasting"
 
 # Your ASCII art photo - paste yours here
 YOUR_ASCII_PHOTO = r"""
-     
+                                        ,
+                                        ,
             ,,,_,,,___                     
        _,╔▓██████████████▄;,,_             
       _██████████████████████▄,             
